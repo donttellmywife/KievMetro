990c2d47f1b57f27aa87770ddee8f0187271d50c
Vladimir Puchkov <Volodymyr.Puchkov@playtech.com>
2016-12-14 12:32:01 +0200

    add test support

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var metro = require('./index');
+
+describe('Kiev Metro', function () {
+    it('module should work', function () {
+        expect(true).to.be.true;
+    });
+});
